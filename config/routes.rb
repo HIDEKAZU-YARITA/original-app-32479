5d58fec057130a9f3ba6b068a58fa621f9113897
HIDEKAZU-YARITA <hiyarita@nifty.com>
2021-01-08 18:57:50 +0900

    動作確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'customers/index'
   devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "customers#index"
+
 end
