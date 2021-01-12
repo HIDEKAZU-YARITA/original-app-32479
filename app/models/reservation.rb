f1b39a7f2a5d84c657f2a4298734e8dba9796fca
HIDEKAZU-YARITA <hiyarita@nifty.com>
2021-01-12 16:55:34 +0900

    予約情報の登録

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -15,8 +15,25 @@ class Reservation < ApplicationRecord
     validates :staff_id
   end
 
-  def date_before_start
-    return if start_time.blank?
-    errors.add(:start_time, "は今日以降のものを選択してください") if start_time < Date.today
+  def self.check(reservation)
+    @reservations = Reservation.where(staff_id: reservation.staff_id)
+    business_hours_end_time = DateTime.new(reservation.start_time.year, reservation.start_time.mon, reservation.start_time.day, 18, 00)
+    break_flag = 0
+    if reservation.start_time < DateTime.now    # 過去
+      break_flag = 4
+    elsif reservation.start_time.wday == 2      # 火曜日
+      break_flag = 3
+    elsif reservation.end_time > business_hours_end_time    # 終了時刻が営業時間外
+      break_flag = 2
+    else
+      unless @reservations.blank?
+        @reservations.each do |f|
+          unless reservation.start_time >= f.end_time || reservation.end_time <= f.start_time   # 予約あり
+            break_flag = 1
+          end
+        end
+      end
+    end
+    return break_flag
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   }
   
   root to: "customers#index"
-  resources :reservations, only: :new
+  resources :reservations, only: [:new, :create]
 end
