9d33b2bedb66a3db608ba36c820d7b2e44aa4081
HIDEKAZU-YARITA <hiyarita@nifty.com>
2021-01-15 16:30:57 +0900

    予約情報の更新

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,15 +1,14 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_customer!
   before_action :set_staffs, only: [:index, :show]
+  before_action :set_reservation, only: [:show, :edit]
+  before_action :set_end_time, only: [:create, :update]
 
   def new
     @reservation = Reservation.new
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
-    @menu = Menu.find(@reservation.menu_id)
-    @reservation.end_time = @reservation.start_time + @menu.time * 60 * 60
     return_value = Reservation.check(@reservation)
     if return_value == 0
       if @reservation.save
@@ -38,11 +37,35 @@ class ReservationsController < ApplicationController
   end
 
   def show
-    @reservation = Reservation.find(params[:id])
   end
 
   def edit
-    @reservation = Reservation.find(params[:id])
+    redirect_to reservations_path(id: @reservation.staff.id) if @reservation.start_time < DateTime.now
+  end
+
+  def update
+    @pre_reservation = Reservation.find(params[:id])
+    return_value = Reservation.check(@reservation)
+    if return_value == 0
+      if @reservation.save
+        @pre_reservation.destroy
+        redirect_to reservations_path(id: @reservation.staff.id)
+      else
+        render :edit
+      end
+    elsif return_value == 1
+      @reservation.errors.add(:start_time, 'is already scheduled.')
+      render :edit
+    elsif return_value == 2
+      @reservation.errors.add(:end_time, 'is outside business hours.')
+      render :edit
+    elsif return_value == 3
+      @reservation.errors.add(:start_time, 'is holiday.')
+      render :edit
+    elsif return_value == 4
+      @reservation.errors.add(:start_time, 'is the past.')
+      render :edit
+    end
   end
 
   private
@@ -53,5 +76,15 @@ class ReservationsController < ApplicationController
 
   def set_staffs
     @staffs = Staff.where.not(id: 0)
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def set_end_time
+    @reservation = Reservation.new(reservation_params)
+    @menu = Menu.find(@reservation.menu_id)
+    @reservation.end_time = @reservation.start_time + @menu.time * 60 * 60
   end
 end
