695e5d60eec8a92361500967c336534d3e38bcfc
HIDEKAZU-YARITA <hiyarita@nifty.com>
2021-01-18 18:48:13 +0900

    お客様情報詳細ページの追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,8 @@
 class CustomersController < ApplicationController
-  def index
+  before_action :authenticate_customer!
+
+  def show
+    @customer = Customer.find(current_customer.id)
+    @reservations = Reservation.where(customer_id: current_customer.id).limit(5).order(start_time: 'DESC')
   end
 end
